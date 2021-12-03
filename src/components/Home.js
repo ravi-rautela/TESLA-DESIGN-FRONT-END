aa76f48f606adb05641800e51fc411d38857cf5c
Ravi Rautela <ranvi44rautela@gmail.com>
2021-12-03 21:25:40 +0530

    ADD: Section onto the Tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,60 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section />
+            <Section
+                title="Model S"
+                discription="Order Online for Touchless Delivery"
+                backgroundImg="model-s.jpg"
+                leftButtonText="CUSTOM ORDER"
+                rightButtonText="EXITING INVENTORY"
+            />
+            <Section
+                title="Model Y"
+                discription="Order Online for Touchless Delivery"
+                backgroundImg="model-y.jpg"
+                leftButtonText="CUSTOM ORDER"
+                rightButtonText="EXITING INVENTORY"
+            />
+            <Section
+                title="Model 3"
+                discription="Order Online for Touchless Delivery"
+                backgroundImg="model-3.jpg"
+                leftButtonText="CUSTOM ORDER"
+                rightButtonText="EXITING INVENTORY"
+            />
+            <Section
+                title="Model X"
+                discription="Order Online for Touchless Delivery"
+                backgroundImg="model-x.jpg"
+                leftButtonText="CUSTOM ORDER"
+                rightButtonText="EXITING INVENTORY"
+            />
+            <Section
+                title="Lowest Csot Solar Panels in America"
+                discription="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftButtonText="Order Now"
+                rightButtonText="Learn More"
+            />
+
+            <Section
+                title="Solar For New Roofs"
+                discription="Soler Roof Costs Less Then a New Roof Plus Solar Panels"
+                backgroundImg="solar-roof.jpg"
+                leftButtonText="Order Now"
+                rightButtonText="Learn More"
+
+            />
+            <Section
+                title="Accessories"
+                discription=""
+                backgroundImg="accessories.jpg"
+                leftButtonText="Shop Now"
+
+
+            />
+
+
         </Container>
     )
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section(props) {
     return (
-        <Wrap>
+        <Wrap bgImage={props.backgroundImg}>
 
             <TestItem>
-                <h1>Model S</h1>
-                <p>Order Online for Touchless Delivery.</p>
+                <h1>{props.title}</h1>
+                <p>{props.discription}</p>
             </TestItem>
-            <ButtonGroup>
-                <LeftButton>
-                    Custom Order
-                </LeftButton>
-                <RightButton>
-                    Exiting Inventory
-                </RightButton>
-            </ButtonGroup>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {props.leftButtonText}
+                    </LeftButton>
+                    {props.rightButtonText &&
+                        <RightButton>
+                            {props.rightButtonText}
+                        </RightButton>
+                    }
+
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -28,7 +34,8 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('./images/model-s.jpg');
+    // background-image: url('./images/model-s.jpg');
+    background-image:${props => `url("/images/${props.bgImage}")`};
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -41,8 +48,11 @@ const TestItem = styled.div`
 `
 const ButtonGroup = styled.div`
     display: flex;
-    margin-bottom: 30px; 
-  
+    margin-bottom: 10px;
+    @media (max-width: 768px){
+        flex-direction: column;
+       
+    } 
    
 `
 const LeftButton = styled.div`
@@ -58,9 +68,23 @@ const LeftButton = styled.div`
     text-transform: uppercase;
     font-size: 12px;
     cursor: pointer;
-    margin-left:24px;
+    margin:8px;
 
 `
 const RightButton = styled(LeftButton)`
+    background:white;
+    opacity:0.65;
+    color:black;
+
+`
+const DownArrow = styled.img`
+    height: 30px;
+    overflow-x:hidden;
+    animation: animateDown infinite 1.5s;
+   
+`
+const Buttons = styled.div`
+    display: flex;
+    flex-direction: column;
 
 `
